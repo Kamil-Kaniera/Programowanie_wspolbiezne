d28617430f25b134a5f89a48c40945dec3f54b1c
Kamil Kaniera 247689 <247689@edu.p.lodz.pl>
2024-04-12 23:51:00 +0200

    Stage 1 - change solution structure and presentation layer v1

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using ViewModel;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
